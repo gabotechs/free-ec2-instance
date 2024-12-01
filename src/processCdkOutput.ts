b9f72e1976baf1607c1cc98edc62cc12f7c29b91
gabotechs <gb.mt.me@gmail.com>
2024-12-01 16:49:13 +0100

    Fix stack names

--- src/processCdkOutput.ts
+++ src/processCdkOutput.ts
@@ -2,10 +2,10 @@ import * as fs from 'fs'
 
 const content = fs.readFileSync('cdk-output.json').toString()
 const decoded = JSON.parse(content)
-if ('FreeEC2Stack' in decoded && 'ConnectScript' in decoded.FreeEC2Stack) {
-  fs.writeFileSync('connect.sh', decoded.FreeEC2Stack.ConnectScript)
+if ('NvidiaRapidsEC2Stack' in decoded && 'ConnectScript' in decoded.NvidiaRapidsEC2Stack) {
+  fs.writeFileSync('connect.sh', decoded.NvidiaRapidsEC2Stack.ConnectScript)
 }
 
-if ('FreeEC2Stack' in decoded && 'ScpScript' in decoded.FreeEC2Stack) {
-  fs.writeFileSync('scp.sh', decoded.FreeEC2Stack.ScpScript)
+if ('NvidiaRapidsEC2Stack' in decoded && 'ScpScript' in decoded.NvidiaRapidsEC2Stack) {
+  fs.writeFileSync('scp.sh', decoded.NvidiaRapidsEC2Stack.ScpScript)
 }
